ccf970e1738eeae0dbc33d0496a87518408076cb
nayana111franco <nayanafranco111@gmail.com>
2026-05-20 13:45:19 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 "Testing CI/CD"
 """Main FastAPI application for MediBook."""
-//hello testing ci/cd
+""" Testing the code for CI/CD"""
 from __future__ import annotations
 
 from datetime import datetime, timedelta
